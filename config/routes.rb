db33d1379678ecefc0158ff0214c313311fb94c8
Cliff <cliff@codesprout.com>
2022-12-19 11:08:46 -0500

    Adds admin users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resource :profile, only: [ :edit, :update ]
+
+  namespace :admin do
+    resources :users, only: [ :index, :show ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
