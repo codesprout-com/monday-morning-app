9918b13dab67256a1dcbee210e66794e937a617f
Cliff <cliff@codesprout.com>
2023-01-11 09:49:34 -0500

    Fixes error

--- app/workers/room_assigner_worker.rb
+++ app/workers/room_assigner_worker.rb
@@ -2,7 +2,7 @@ class RoomAssignerWorker
   include Sidekiq::Job
 
   def perform
-    room_assigner = RoomAssigner.next_week
+    room_assigner = RoomAssigner.new
     room_assigner.perform_assignments
   end
 end
